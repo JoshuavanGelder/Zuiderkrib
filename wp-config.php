3be38c89f9d8bc4704afa59248a1e9fe4e13ec0a
JoshuavanGelder <75835803+JoshuavanGelder@users.noreply.github.com>
2022-04-13 13:13:13 +0200

    ADD: hotelier plugin

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'zuiderkrib' );
+define( 'DB_NAME', 'zuiderkrib-booking' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
